43bba5e164b561721ed01c199547d0b015b6da12
akozdev <akoz@hey.com>
2021-10-23 15:40:20 +0200

    Add zadanie_2_4.c

---
+++ zadanie_2_4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void displayPanieJanie() {
+  printf("Panie Janie!\n");
+}
+
+void displayRanoWstan() {
+  printf("Rano Wstan!\n");
+}
+
+int main() {
+  displayPanieJanie();
+  displayPanieJanie();
+  displayRanoWstan();
+
+  return 0;
+}
