f90b4ff5b25af66b7965088b4ebd9cf09df8c92b
akozdev <akoz@hey.com>
2021-10-23 16:30:20 +0200

    Add zadanie_3_3.c

---
+++ zadanie_3_3.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main() {
+  Beep(750, 800);
+  printf("Sally, przerazona niespodziewanym odglosem, krzyknela \"A niech mnie, co to bylo!?\"");
+  return 0;
+}
